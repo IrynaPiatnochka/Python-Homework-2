55f586d208984bbe5255a0c8701d6dcc84a2b211
Iryna-Piatnochka <ivpiatnochka@gmail.com>
2024-04-10 21:41:53 -0500

    updated code

--- Determining leap year.py
+++ Determining leap year.py
@@ -2,7 +2,7 @@
 
 year = int(input("Enter a year: "))
 
-if (year%100==0 and year%400==0) or (year%100!=0 and year%4==0): 
+if (year/100==0 and year/400==0) or (year/100!=0 and year/4==0): 
     print('This is a leap year.') 
 else: 
     print('This is not a leap year.') 
